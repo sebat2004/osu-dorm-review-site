e3d5150490080ff51d370f35468a9e4799b133d3
Sebastian <121001035+sebat2004@users.noreply.github.com>
2023-05-20 03:49:42 -0700

    Accidentally published API key
    Yikes

--- website/ratings.py
+++ website/ratings.py
@@ -3,7 +3,7 @@ import requests as requests
 
 ratings = Blueprint("ratings", __name__) 
 
-url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJl-61hrlAwFQRSsuaxoMTm7I&fields=reviews%2Crating&key=AIzaSyBYII-GqPLWO7JRqsOpCd6Z7JwvFQq9g54"
+url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJl-61hrlAwFQRSsuaxoMTm7I&fields=reviews%2Crating&key=***"
 
 payload= {}
 headers = {}
